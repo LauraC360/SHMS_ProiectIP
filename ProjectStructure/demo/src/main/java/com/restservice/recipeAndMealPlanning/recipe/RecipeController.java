1f0351aa9234a0f3e699b8ae749237df482957ee
LauraC360 <63462151+LauraC360@users.noreply.github.com>
2024-04-11 00:11:15 +0300

    Update RecipeController.java

--- ProjectStructure/demo/src/main/java/com/restservice/recipeAndMealPlanning/recipe/RecipeController.java
+++ ProjectStructure/demo/src/main/java/com/restservice/recipeAndMealPlanning/recipe/RecipeController.java
@@ -1,28 +1,3 @@
-//package com.restservice.recipeAndMealPlanning.recipe;
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/recipes")
-//public class RecipeController {
-//
-//    private List<Recipe> recipes = new ArrayList<>();
-//    private Recipe recipe = new Recipe(1, "Pasta", "Pasta with tomato sauce", null, 4.7f, null);
-//
-//    @PostMapping
-//    public void createRecipe() {
-//        recipes.add(recipe);
-//    }
-//
-//    @GetMapping
-//    public List<Recipe> getAllRecipes() {
-//        return recipes;
-//    }
-//}
-
 package com.restservice.recipeAndMealPlanning.recipe;
 
 import org.springframework.web.bind.annotation.*;
