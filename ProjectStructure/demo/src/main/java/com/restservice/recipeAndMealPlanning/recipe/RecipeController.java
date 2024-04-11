64a3323b5160af942f749bcea1ed849f43face8d
LauraC360 <63462151+LauraC360@users.noreply.github.com>
2024-04-11 11:05:53 +0300

    Update RecipeController.java

--- ProjectStructure/demo/src/main/java/com/restservice/recipeAndMealPlanning/recipe/RecipeController.java
+++ ProjectStructure/demo/src/main/java/com/restservice/recipeAndMealPlanning/recipe/RecipeController.java
@@ -2,13 +2,14 @@ package com.restservice.recipeAndMealPlanning.recipe;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/api/recipes")
+@RequestMapping("/recipes")
 public class RecipeController {
 
-    private static final Recipe defaultRecipe = new Recipe(1, "Pasta", "Pasta with tomato sauce", new ArrayList<RecipeStep>(), 4.7f, null);
+    private static final Recipe defaultRecipe = new Recipe(1, "Pasta", "Pasta with tomato sauce", new ArrayList<RecipeStep>(), 4.7f, new File("/img/pasta-with-simple-tomato-sauce.jpeg"));
 
     @GetMapping("/default")
     public Recipe getDefaultRecipe() {
